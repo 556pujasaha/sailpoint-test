a9fbcf1f7538e40e32fdfd52f99308dab1b8dd67
556pujasaha <119627057+556pujasaha@users.noreply.github.com>
2023-02-22 19:26:08 +0530

    Add files via upload

---
+++ SAILPOINT_TASK.ps1
@@ -0,0 +1,48 @@
+﻿# Import required modules
+Import-Module -Name Microsoft.PowerShell.Utility
+Import-Module -Name Microsoft.PowerShell.Management
+Import-Module -Name GitHub
+
+# Set variables
+$repositoryName = "octocat/Hello-World"
+$startDate = (Get-Date).AddDays(-7)
+$endDate = Get-Date
+$emailTo = "556pujasaha@gmail.com"
+$emailFrom = "pujasaha556@gmail.com"
+$emailSubject = "GitHub Pull Request Summary Report"
+
+# Authenticate with GitHub API
+$token = "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"
+Set-GitHubAuthentication -AccessToken $token
+
+# Retrieve pull requests
+$pullRequests = Get-GitHubPullRequest -Repository $repositoryName -State all -Since $startDate -Until $endDate
+
+# Filter pull requests by state
+$closedPullRequests = $pullRequests | Where-Object { $_.state -eq "closed" }
+$openPullRequests = $pullRequests | Where-Object { $_.state -eq "open" }
+$mergedPullRequests = $pullRequests | Where-Object { $_.state -eq "merged" }
+
+# Format email body
+$emailBody = @"
+GitHub Pull Request Summary Report
+
+Closed Pull Requests
+---------------------
+$($closedPullRequests.Count) pull requests closed in the last week.
+
+Open Pull Requests
+-------------------
+$($openPullRequests.Count) pull requests currently open.
+
+Merged Pull Requests
+---------------------
+$($mergedPullRequests.Count) pull requests merged in the last week.
+"@
+
+# Send email
+Send-MailMessage -To $emailTo -From $emailFrom -Subject $emailSubject -Body $emailBody
+
+# Print email details to console
+Write-Host "Email sent to $emailTo from $emailFrom with subject '$emailSubject' and body:"
+Write-Host $emailBody
